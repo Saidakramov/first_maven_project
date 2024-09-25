e03a91a4b464d45c825e3524274b02edbaf0011b
Akbar <akbarsaidakramov@gmail.com>
2024-09-25 11:07:11 -0700

    adding first maven project

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Apple");
+        System.out.println("Banana");
+        System.out.println("Orange");
+        System.out.println("Pear");
+        System.out.println("Grapes");
+        System.out.println("Cucumber");
+        System.out.println("Tomato");
+        System.out.println("Cilantro");
+        System.out.println("Eggs");
+        System.out.println("Milk");
+    }
+}
